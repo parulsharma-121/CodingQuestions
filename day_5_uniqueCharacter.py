b64117bad750ce65426767b5f6ed87d05a436613
parulsharma-121 <contactparulsharma11@gmail.com>
2020-03-29 20:46:52 +0530

    day5

---
+++ day_5_uniqueCharacter.py
@@ -0,0 +1,18 @@
+def firstUniqChar(s: str) -> int:
+        my_hash=dict()
+        for i in range(len(s)):
+            if(s[i] in my_hash):
+                my_hash[s[i]] = -1
+            else:
+                my_hash[s[i]] = i
+        
+        print(my_hash)
+        for x in my_hash.keys():
+            if(my_hash[x]>-1):
+                return  my_hash[x]
+        
+        return -1
+
+s = 'leetcode'
+
+print(firstUniqChar(s))
