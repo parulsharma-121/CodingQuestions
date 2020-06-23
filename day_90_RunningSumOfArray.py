a94d0baa9b1bebaae605c4dfc88ea0d9e9e3dcdf
parulsharma-121 <contactparulsharma11@gmail.com>
2020-06-23 22:22:17 +0530

    day90

---
+++ day_90_RunningSumOfArray.py
@@ -0,0 +1,31 @@
+'''
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+
+'''
+def runningSum(nums):
+    res = []
+    curr = 0
+    for i in nums:
+        curr += i
+        res.append(curr)
+    return res
+
+nums = [1,2,3,4]
+print(runningSum(nums))
+            
