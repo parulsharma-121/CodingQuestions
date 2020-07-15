07449fbf456d32e93e1cb722149a35f7fefb31fe
parulsharma-121 <contactparulsharma11@gmail.com>
2020-07-15 21:58:47 +0530

    day113

--- day_113_TwoSumII.py
+++ day_113_TwoSumII.py
@@ -11,7 +11,7 @@ Example:
 
 Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
-Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
 '''
 def twoSum(numbers, target):
