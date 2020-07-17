ea6f275d42b6f3e2af0c0d5c7fc0c165430cec64
parulsharma-121 <contactparulsharma11@gmail.com>
2020-07-17 21:53:28 +0530

    day115

---
+++ day_115_DistributeCandies.py
@@ -0,0 +1,16 @@
+'''
+Given an integer array with even length, where different numbers in this array represent different kinds of candies. Each number means one candy of the corresponding kind. You need to distribute these candies equally in number to brother and sister. Return the maximum number of kinds of candies the sister could gain.
+Example 1:
+Input: candies = [1,1,2,2,3,3]
+Output: 3
+Explanation:
+There are three different kinds of candies (1, 2 and 3), and two candies for each kind.
+Optimal distribution: The sister has candies [1,2,3] and the brother has candies [1,2,3], too. 
+The sister has three different kinds of candies. 
+
+
+'''
+def distributeCandies(candies):
+    return min(len(set(candies)),len(candies)//2)
+candies = [1,1,2,2,3,3]
+print(distributeCandies(candies))
