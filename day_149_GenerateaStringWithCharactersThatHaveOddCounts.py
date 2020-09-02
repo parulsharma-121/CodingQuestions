1f1b73e06929352c9ee815520f7c39ae5a9c3c0d
parulsharma-121 <contactparulsharma11@gmail.com>
2020-09-02 22:31:12 +0530

    day149

---
+++ day_149_GenerateaStringWithCharactersThatHaveOddCounts.py
@@ -0,0 +1,23 @@
+'''
+Given an integer n, return a string with n characters such that each character in such string occurs an odd number of times.
+
+The returned string must contain only lowercase English letters. If there are multiples valid strings, return any of them.  
+
+ 
+
+Example 1:
+
+Input: n = 4
+Output: "pppz"
+Explanation: "pppz" is a valid string since the character 'p' occurs three times and the character 'z' occurs once. Note that there are many other valid strings such as "ohhh" and "love".
+
+'''
+def generateTheString(n):
+    if(n % 2 == 0):
+        return 'a'*(n-1) + 'b'
+    
+    else:
+        return 'a'*n
+n = 4
+print(generateTheString(n))
+        
