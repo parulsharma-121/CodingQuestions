23a8d4caa5c54766de1b006e23d098e2f0c837a1
parulsharma-121 <contactparulsharma11@gmail.com>
2020-09-26 22:30:09 +0530

    day164

---
+++ day_164_ReverseBits.py
@@ -0,0 +1,31 @@
+'''
+Reverse bits of a given 32 bits unsigned integer.
+
+ 
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: 11111111111111111111111111111101
+Output: 10111111111111111111111111111111
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+
+'''
+def reverseBits(n):
+    result = 0
+    i = 0
+    while(i<32):
+        result <<=1 
+        result |= (n&1) 
+        n >>= 1 
+        i+=1
+    return result
+
+n = 43261596
+print(reverseBits(n))
+
