71beaaf8a5b859299028771a441ac90489838120
parulsharma-121 <contactparulsharma11@gmail.com>
2020-10-08 20:14:02 +0530

    day172

---
+++ day_172_DefangingAnIPAddress.py
@@ -0,0 +1,22 @@
+'''
+Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+
+
+'''
+def defangIPaddr(address):
+    return '[.]'.join(address.split('.'))
+address = "1.1.1.1"
+print(defangIPaddr(address))
